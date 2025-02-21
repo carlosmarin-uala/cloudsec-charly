b1a125c793c3acc980d06ad9574856cbd89b135e
CarlosM <carlos.marin@uala.com.ar>
2025-02-21 10:55:23 -0300

    update scp deploy

--- Organizations-SCP/vars.tf
+++ Organizations-SCP/vars.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "policy_name" {
   description = "Nombre de la política de la organización"
   type        = string
-  default     = "Prueba-SCP-ByTerra-Charly"
+  default     = "Prueba-Crear-SCP-ByTerra-Charly"
 }
 
 variable "policy_description" {
