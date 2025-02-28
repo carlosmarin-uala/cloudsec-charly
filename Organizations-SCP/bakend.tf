3ddb49394cfcda2ccef255f53e93e95a0136bb8a
CarlosM <carlos.marin@uala.com.ar>
2025-02-28 13:57:04 -0300

    update scp deploy

--- Organizations-SCP/bakend.tf
+++ Organizations-SCP/bakend.tf
@@ -4,6 +4,6 @@ terraform {
     encrypt        = true                       # Habilita la encriptación para el estado
     key            = "cloudsecurity/405941243308/terraform.tfstate"  # Ruta del estado en S3
     region         = "us-east-1"                # Región de S3 y DynamoDB
-    dynamodb_table = "charly-terraform-locks"                # Nombre de la tabla DynamoDB para el bloqueo
+    #dynamodb_table = "charly-terraform-locks"                # Nombre de la tabla DynamoDB para el bloqueo
   }
 }
