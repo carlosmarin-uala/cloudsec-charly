45c7764a630bcd60b175b08927ead3edc7a95854
CarlosM <carlos.marin@uala.com.ar>
2025-03-07 10:49:31 -0300

    update scp deploy

--- Organizations-SCP/main.tf
+++ Organizations-SCP/main.tf
@@ -7,14 +7,23 @@ resource "aws_organizations_policy" "denegar_creacion_usuarios_iam" {
   description = var.policy_description
   type        = var.policy_type
 
-  content = jsonencode({
+  content = jsonencode(
+    {
     "Version": "2012-10-17",
     "Statement": [
       {
         "Effect": "Deny",
         "Action": var.iam_action
-        "Resource": "*"
+        "Resource": "*",
+        "Condition": {
+        "StringNotEquals": {
+          "identitystore:UserId": [
+            "9067073511-165e9956-13b0-4df4-b8b0-365e6cb056e5"
+          ]
+        }
+        }
       }
     ]
-  })
+    }
+  )
 }
